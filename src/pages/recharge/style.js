8449a4c349e11ce7a92a06e3a69432fc3ea5c63b
chengqi77 <2113265828@qq.com>
2018-01-22 22:55:13 +0800

    封装单选框

--- src/pages/recharge/style.js
+++ src/pages/recharge/style.js
@@ -1,34 +1,66 @@
 export default {
-    index:{
-        height:"100%",
-        width:"100%",
-        paddingLeft:40,
-        paddingRight: 40,
-       
-        backgroundColor: '#fff',
+    container: {
+        flex: 1,
+        paddingLeft: 20,
+        paddingRight: 20,
+        backgroundColor: '#f9f9f9',
     },
-    indexipnut:{
-        flexDirection:"row",
-        alignItems:"center",
-        height:45,
-        borderColor:"red",
+    rechargeLabel: {
+        lineHeight: 70,
+        color:"#333",
+    },
+    inputContainer: {
+        flexDirection: "row",
+        height: 60,
+        paddingTop: 10,
+        paddingBottom: 10,
+        borderColor: "#1e89e4",
         borderWidth: 1,
-        borderRadius:5,
+        borderRadius: 5,
     },
-    indexipnt:{
-        flex:1,
-        height:"80%",
-        alignItems:"center",
-        width:"80%",
-        lineHeight:45,
+    input: {
+        width: '80%',
+        borderRightWidth: 1,
+        borderColor: "#1e89e4"
     },
-    indexy:{
-        width:'20%',
-        height:"80%",
-        alignItems:"center",
-        lineHeight:45,
-        borderColor: 'red',
-        borderLeftWidth:1,
-        paddingLeft:16,
-        
-}}
+    inputLabelWrapper: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    inputLabel:{
+        fontSize:16,
+        color:'#039DEB',
+        fontWeight:"600"
+    },
+    balanceWrapper: {
+        flexDirection: "row-reverse",
+        height: 70,
+      alignItems:'center'
+    },
+    indexipnt: {
+        flex: 1,
+        height: "80%",
+        alignItems: "center",
+        width: "80%",
+        lineHeight: 45,
+    },
+    apply: {
+        flex: 1,
+        height: 50,
+    },
+    checklabel:{
+        lineHeight:20,
+        color:"#333",
+    },
+    indexy: {
+        width: '20%',
+        height: "80%",
+        alignItems: "center",
+        lineHeight: 45,
+        borderColor: '#1e89e4',
+        borderLeftWidth: 1,
+        paddingLeft: 16,
+
+    }
+}
