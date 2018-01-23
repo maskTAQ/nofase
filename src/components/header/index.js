5f83c6156e0d12f3e4b6cb745660f02ae81baac3
masktaq <50889574@qq.com>
2018-01-23 09:48:33 +0800

    添加eslint

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,27 +1,42 @@
-import React ,{Component} from "react";
-import { View,StatusBar } from "react-native";
-//import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { View, StatusBar } from "react-native";
+import PropTypes from "prop-types";
 
+import styles from "./style";
 
-export default class Header extends Component{
-    render() {
-        const barStyle = {
-            backgroundColor:'transparent',
-            barStyle:'light-content'
-        };
-         return (
-          <View style={[styles.container, this.props.customStyles.container]}>
-            <StatusBar
-              backgroundColor={barStyle.backgroundColor}
-              translucent={true}
-              barStyle={barStyle.barStyle}
-            />
-            <View style={styles.navigationContainer}>
-              {this.props.left}
-              {this.renderTitle()}
-              {this.props.right}
-            </View>
-          </View>
-        );
-      }
-} 
+const Left = () => <View />;
+const Right = () => <View />;
+export default class Header extends Component {
+  static defaultProps = {
+    LeftComponent: Left,
+    RightComponent: Right
+  };
+  static propTypes = {
+    LeftComponent: PropTypes.element,
+    RightComponent: PropTypes.element,
+    style: PropTypes.object,
+    title: PropTypes.string
+  };
+  state = {};
+  render() {
+    const { LeftComponent, RightComponent, title, style } = this.props;
+    const barStyle = {
+      backgroundColor: "transparent",
+      barStyle: "light-content"
+    };
+    return (
+      <View style={[styles.container, style]}>
+        <StatusBar
+          backgroundColor={barStyle.backgroundColor}
+          translucent={true}
+          barStyle={barStyle.barStyle}
+        />
+        <View style={styles.navigationContainer}>
+          <LeftComponent />
+          {title}
+          <RightComponent />
+        </View>
+      </View>
+    );
+  }
+}
--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -1,15 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import { Image } from "react-native";
 import PropTypes from "prop-types";
 
-
-const Icon = ({ size, source}) => {
-    return (
-        <Image style={style} source={source}></Image>
-    );
+const Icon = ({ size, source }) => {
+  return <Image style={{ width: size, height: size }} source={source} />;
 };
 Icon.propTypes = {
-    size:PropTypes.number,
-    source:PropTypes.number
+  size: PropTypes.number,
+  source: PropTypes.number
 };
 export default Icon;
