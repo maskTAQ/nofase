16feb0cadabd4a22c81d3fc721e4954b231b8a83
masktaq <50889574@qq.com>
2018-01-26 14:26:02 +0800

    完成评分组件

---
+++ src/components/star-score/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+import PropTypes from "prop-types";
+
+import { Button, Icon } from "src/components";
+
+import styles from "./style";
+
+export default class StarScore extends Component {
+  static defaultProps = {
+    totalScore: 5,
+    operable: true
+  };
+  static propTypes = {
+    totalScore: PropTypes.number,
+    currentScore: PropTypes.number,
+    operable: PropTypes.bool
+  };
+  state = {
+    totalScore: 5, // 总分值
+    currentScore: 3 // 分值
+  };
+  renderChildren(i) {
+    const { currentScore } = this.props;
+    if (i < currentScore) {
+      return <Icon size={14} source={require("全")} />;
+    }
+    if (Math.ceil(i) === Math.floor(currentScore) + 1 && !/^-?\d+$/.test(i)) {
+      return <Icon size={14} source={require("半全")} />;
+    }
+
+    return <Icon size={14} source={require("none")} />;
+  }
+  renderBody() {
+    const { totalScore, operable } = this.props;
+    const images = [];
+    for (let i = 1; i <= totalScore; i++) {
+      const commonProps = {
+        key: i,
+        children: this.renderChildren(i),
+        style: styles.item
+      };
+      images.push(
+        operable ? (
+          <Button
+            onPress={() => {
+              this.onChangeScore(i);
+            }}
+            {...commonProps}
+          />
+        ) : (
+          <View {...commonProps} />
+        )
+      );
+    }
+    return images;
+  }
+  render() {
+    return <View style={styles.container}>{this.renderBody()}</View>;
+  }
+}
