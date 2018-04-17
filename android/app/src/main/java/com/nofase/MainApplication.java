cab1f55e1672b828c5052dfc5690890aa365b59d
masktaq <50889574@qq.com>
2018-04-17 20:52:53 +0800

    配置android-微信key

--- android/app/src/main/java/com/nofase/MainApplication.java
+++ android/app/src/main/java/com/nofase/MainApplication.java
@@ -60,7 +60,7 @@ public class MainApplication extends Application implements ReactApplication {
 
   // 配置平台key、secret信息
   {
-    PlatformConfig.setWeixin("wx083bf496cbc48aec", "750e9075fa521c82274a9d548c399825");
+    PlatformConfig.setWeixin("wxa0f45f747e934b0b", "4edbdf12d2e975963cfecd35996f9052");
     PlatformConfig.setQQZone("1106207359", "3JjbG8aXMuh5w0sV");
     PlatformConfig.setSinaWeibo("2733400964", "fac50980a44e3e3afd4bc968ea572887", "www.baidu.com");
   }
